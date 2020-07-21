99d3878c7325d725c3af3432883b175dd7e907b2
Mocktar <mocktarissa@gmail.com>
2020-07-21 16:48:22 +0300

    Added useReducer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
-imprt useFetchJobs from './useFetchJobs'
+import useFetchJobs from './useFetchJobs';
+import {container} from  'react-bootstrap'
 function App(){
   const {jobs, loading , error} = useFetchJobs()
+  return(
+    <container>
+      {loading && <h1>Loading ....</h1>}
+      {error && <h1>Error Plead refresh the page</h1>}
+      {jobs.length}
+    </container>
+  )
 }
 export default App;
--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -1,3 +1,10 @@
+import axios from 'axios';
+import {useReducer} from 'react'
 export default function useFetchJobs(params,page){
-
+    const [state,dispatch] =useReducer(reducer,{jobs:[],loading:true})
+    return {
+        jobs:[],
+        error:true,
+        loading:false,
+    }
 }
